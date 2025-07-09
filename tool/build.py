7a2a100b1cd765e5c18b37c2a22e53fdd861001f
karoterra <karoterra2008@gmail.com>
2025-07-09 20:33:39 +0900

    :bookmark: v1.0.0

--- tool/build.py
+++ tool/build.py
@@ -22,7 +22,7 @@ def process_script(path: Path, info: str, version: str, author: str, params: dic
 
 def main():
     AUTHOR = "karoterra"
-    VERSION = "v0.1.0"
+    VERSION = "v1.0.0"
 
     SHADER_brettel1997 = Path("script/brettel1997.hlsl").read_text(encoding="utf-8")
     SHADER_vienot1999 = Path("script/vienot1999.hlsl").read_text(encoding="utf-8")
